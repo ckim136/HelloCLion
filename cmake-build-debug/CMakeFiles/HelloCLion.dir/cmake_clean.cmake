ffe477a4283ed6b137a276e0b3f45422814a24cb
Chaeeun Kim <kimemily1227@gmail.com>
2019-09-09 14:40:03 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/HelloCLion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloCLion.dir/main.cpp.o"
+  "HelloCLion.pdb"
+  "HelloCLion.exe"
+  "libHelloCLion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
